f6abdb01f7bc70884397cb25ed57bda90fae3954
Mateusz Findeisen <mfindeis@mion.elka.pw.edu.pl>
2019-03-15 21:50:16 +0100

    changed argument name to reflect what function is actually doing more precisely, also get_out_nested_list_from_string allows to get multiple-dimensional points

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
-def get_out_nested_list_from_string(string):
+def get_out_nested_list_from_string(ints_list):
+    print(len(ints_list))
     our_list = list()
     for i in range(3):
         temp_list = []
-        for j in range(3 if i < 2 else 1):
-            temp_list.append(string[3 * i + j])
+        for j in range((len(ints_list) - 1) / 2 if i < 2 else 1):
+            temp_list.append(ints_list[3 * i + j])
         our_list.append(temp_list)
     return our_list
