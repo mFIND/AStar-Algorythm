a010ea8f9faf74990ea6e3096752038c07a0daa2
Mateusz Findeisen <mfindeis@mion.elka.pw.edu.pl>
2019-03-18 15:49:02 +0100

    Added base for program-user interaction

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import os
+
+from pip._vendor.distlib.compat import raw_input
+
 from read_list_from_file import *
 from generate_graph_from_list import *
 
-from util import get_out_nested_list_from_list as list_to_nested_list
+TEMPORARY_FILENAME = raw_input("Input filename: ")
 
-TEMPORARY_FILENAME = "~temp_file.txt"
 
-edges = [
-        [[0, 0, 0], [4, 5, 6], 4],
-        [[4, 5, 6], [1, 3, 9], 6],
-        [[4, 7, 1], [1, 3, 9], 3],
-        [[4, 7, 1], [4, 0, 4], 9],
-        ]
+if True is True:  # TODO delete this block of code
+    edges = [
+            [[0, 0, 0], [4, 5, 6], 4],
+            [[4, 5, 6], [1, 3, 9], 6],
+            [[4, 7, 1], [1, 3, 9], 3],
+            [[4, 7, 1], [4, 0, 4], 9],
+            ]
+    try:
+        with open(TEMPORARY_FILENAME, "x") as temp_file:  # write, not append
+            for i in range(len(edges)):
+                temp_file.write(str(edges[i])+'\n')
+    except FileExistsError:
+        print("File already exists! Please run program once again with different file name.")
+        exit(1)
 
-with open(TEMPORARY_FILENAME, "w") as temp_file:  # write, not append
-    for i in range(len(edges)):
-        temp_file.write(str(edges[i])+'\n')
 
 list_reader = ListReader(TEMPORARY_FILENAME)
 other_edge = list_reader.get_list()
@@ -33,7 +40,7 @@ del graphGenerator
 
 
 # file is automatically closed, because it was opened with ''''with'''', so we can safely remove it now
-os.remove(TEMPORARY_FILENAME)
+#os.remove(TEMPORARY_FILENAME)
 
 
 print("Let's compare initial and read-from-file lists:")
