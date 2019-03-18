e992e63d0bfc466295af9b08cb39f8126d16c39d
Mateusz Findeisen <mfindeis@mion.elka.pw.edu.pl>
2019-03-18 10:20:47 +0100

    added concept for method of creating graph

--- generate_graph_from_list.py
+++ generate_graph_from_list.py
@@ -10,3 +10,23 @@ class GraphGenerator:
 
     def compute_graph(self):
         return self.__list[0][0], self.__list[self.__y_dim - 1][1]
+
+
+"""
+    @David, tutaj mam pomysł na to, jak graf generować:
+    
+    zapisujemy pkt startowy i początkowy:
+    self.__list[0][0], self.__list[self.__y_dim - 1][1]
+    
+    tworzymy mapę(C++)/dictionary(Python), w którym słowo klucz to
+    współrzędne danego pkt (np. [0,0,0], ewentualnie str([0,0,0])),
+    a wartością element (domyślnie przechowywany jako wskaźnik o ile się nie mylę)
+    
+    dzięki czemu będzie można łatwo prze-iterować przez całą listę,
+    i dodać wszystkie zależności między wierzchołkami grafu
+
+    Przy iterowaniu:
+    -> albo pkt o zadanym kluczu istnieje, albo nie, i go tworzymy
+    -> x2, bo 2 pkty na element listy
+    -> w danym elemencie tworzymy powiązanie do elementu sąsiedniego
+"""
