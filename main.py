daa47f26d6ea4e2d6b465529c6fe4ee1b38f8aa9
Mateusz Findeisen <mfindeis@mion.elka.pw.edu.pl>
2019-03-18 23:02:24 +0100

    Added main part of the A* algorithm, which is now functional

--- main.py
+++ main.py
@@ -2,37 +2,44 @@ import os
 
 from pip._vendor.distlib.compat import raw_input
 
+from path_finder import PathFinder
 from read_list_from_file import *
 from generate_graph_from_list import *
 
 TEMPORARY_FILENAME = raw_input("Input filename: ")
 
-
 if True is True:  # TODO delete this block of code
     edges = [
-            [[0, 0, 0], [4, 5, 6], 4],
-            [[4, 5, 6], [1, 3, 9], 6],
-            [[4, 7, 1], [1, 3, 9], 3],
-            [[4, 7, 1], [4, 0, 4], 9],
-            ]
+        [[0, 0, 0], [4, 5, 6], 4],
+        [[4, 5, 6], [1, 3, 9], 6],
+        [[4, 7, 1], [1, 3, 9], 3],
+        [[4, 7, 1], [4, 0, 4], 9],
+    ]
+
     try:
         with open(TEMPORARY_FILENAME, "x") as temp_file:  # write, not append
             for i in range(len(edges)):
-                temp_file.write(str(edges[i])+'\n')
+                temp_file.write(str(edges[i]) + '\n')
     except FileExistsError:
         print("File already exists! Please run program once again with different file name.")
         exit(1)
+    except FileNotFoundError:
+        print("Enter a name next time!")
+        exit(2)
 
 
 list_reader = ListReader(TEMPORARY_FILENAME)
 other_edge = list_reader.get_list()
-graphGenerator = GraphGenerator(other_edge)
 
+del list_reader
+
+graphGenerator = GraphGenerator(other_edge)
 graph = graphGenerator.create_graph()
 start_end_coords = graphGenerator.get_start_and_finish_coordinates()
 
-del list_reader
 del graphGenerator
+
+pathFinder = PathFinder(graph[0], graph[1])
 
 """ lets live that there so we can later check what method is actually faster"""
 # temp_edge = [int(s) for s in temp_str.replace('[', '').replace(']', '').split(',')]
@@ -40,16 +47,14 @@ del graphGenerator
 
 
 # file is automatically closed, because it was opened with ''''with'''', so we can safely remove it now
-#os.remove(TEMPORARY_FILENAME)
-
+os.remove(TEMPORARY_FILENAME)
 
 print("Let's compare initial and read-from-file lists:")
 print("1st: ", type(edges), edges)
 print("3rd: ", type(other_edge), other_edge)
 
 print(start_end_coords)
-print(graph)
-print(graph[0])
-print(graph[0].get_coordinates())
-print(graph[0].get_neighbours())
 
+print("*-" * 15, '*', sep='')
+print(pathFinder.find_path())
+#pathFinder.debug()
---
+++ path_finder.py
@@ -0,0 +1,69 @@
+from vertex import *
+
+
+class PathFinder:
+    __start = None
+    __finish = None
+    __border_points_list = None
+    __extended_points = None
+
+    def __init__(self, start, finish):
+        self.__start = start
+        self.__finish = finish
+        self.__border_points_list = list()
+        self.__border_points_list.append(list([start, 0]))
+        self.__extended_points = list()
+
+    def find_path(self):
+        while len(self.__border_points_list) is not 0:
+            self.__sort_border_points()
+            if self.__extend_vertex():
+                return self.__return_path()
+        return None     # No path found!
+
+    def __return_path(self):
+        string = ""
+        path = self.__border_points_list[0]
+        for i in range(2, len(path)):
+            string += str(path[i].get_coordinates()) + " -> "
+        string += str(path[0].get_coordinates())
+        return string
+
+    def __sort_border_points(self):
+        temp_list = sorted(self.__border_points_list, key=lambda x: x[1] + self.__heuristic_distance(x[0], self.__finish))
+        self.__border_points_list = temp_list
+        return None
+
+    def __extend_vertex(self):
+        if self.__border_points_list[0][0] is not self.__finish:
+            working_elem = self.__border_points_list[0]
+            self.__extended_points.append(working_elem[0])
+            self.__border_points_list.pop(0)                                                  # TODO performance penalty
+            for neighbour in working_elem[0].get_neighbours():
+                if neighbour[0] not in self.__extended_points:
+                    temp_elem = [neighbour[0], working_elem[1] + neighbour[1]] + working_elem[2:] + [working_elem[0]]
+                    self.__border_points_list.append(temp_elem)
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def __heuristic_distance(some_vertex, other_vertex):
+        assert (len(some_vertex.get_coordinates()) == len(other_vertex.get_coordinates()))
+        d = sum([abs(x[0]**2 - x[1]**2) for x in zip(some_vertex.get_coords(), other_vertex.get_coords())]) ** (1/2)
+        return d
+
+
+"""
+    @staticmethod
+    def heuristic_distance(some_vertex, other_vertex):
+        d1 = [x**2 for x in some_vertex.get_coordinates()]
+        d2 = [x**2 for x in other_vertex.get_coordinates()]
+
+        dist = 0
+        for i in range(len(d1)):
+            dist += abs(d1[i] - d2[i])
+        dist **= 1/2
+
+        return dist
+"""
